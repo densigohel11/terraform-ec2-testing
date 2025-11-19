d6fab46d4ce35ae3ab9e532053e774589553144c
densigohel11 <118363820+densigohel11@users.noreply.github.com>
2025-11-19 20:06:17 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
-resource "aws_instance" "demo" {
-  ami           = var.ami_id
+resource "aws_instance" "example" {
+  ami           = var.instance_ami
   instance_type = var.instance_type
-  key_name      = var.key_name
 
   tags = {
     Name = var.instance_name
