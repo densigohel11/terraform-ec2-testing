ebdc3e604316a9f9411f28684b29a3f0dd3c61fd
densigohel11 <118363820+densigohel11@users.noreply.github.com>
2025-11-19 19:54:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,4 @@
-variable "region" {
-  description = "AWS Region"
-  default     = "us-east-1"
-}
-
-variable "instance_ami" {
-  description = "AMI to use for EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-}
-
-variable "instance_name" {
-  description = "Tag name for instance"
-  type = string
-}
+variable "instance_type" {}
+variable "ami_id" {}
+variable "key_name" {}
+variable "instance_name" {}
